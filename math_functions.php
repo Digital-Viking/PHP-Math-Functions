a0a8ba1fa67ad21becb2febfff434003fd878672
David Weston Webb <westwebb@gmail.com>
2019-01-10 13:21:23 -0500

    Create math_functions.php

---
+++ math_functions.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+
+<?php
+
+echo pow(2,7);
+// 2 to the power of 7 = 128
+echo "<br>";
+
+echo rand(1,1000);
+// gives a random number from first to second number
+echo "<br>";
+
+echo sqrt(100);
+// square root of 100 = 10
+echo "<br>";
+
+echo ceil(4.6);
+// rounds up to next number = 5
+echo "<br>";
+
+echo floor(4.6);
+// rounds down to next number = 4
+echo "<br>";
+
+echo round(4.5);
+// rounds up or down depending on number, just like you learned in school
+
+
+?>
+
+</body>
+</html>
